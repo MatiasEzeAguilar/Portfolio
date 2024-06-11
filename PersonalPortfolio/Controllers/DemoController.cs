ac39fe835dfeca2c98d0025b397b5ca8d89231ce
Matias Ezequiel Aguilar <mattiezeaguila12@gmail.com>
2024-06-11 19:41:37 -0300

    new file:   PersonalPortfolio/Controllers/DemoController.cs 	new file:   PersonalPortfolio/Program.cs 	new file:   Portfolio.sln

---
+++ PersonalPortfolio/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+
+    }
+
+}
---
+++ PersonalPortfolio/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BlogContext>(
+       options => options.UseSqlite("Data Source=.;Initial Catalog=PortfolioDb;Integrated Security=True;"));
+
+builder.Services.AddCors(options =>
+{
+    
+    options.AddPolicy("default", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseCors("default");
+app.MapControllers();
+
+app.Run();
